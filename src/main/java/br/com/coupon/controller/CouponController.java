d510f42ad1da667c7ee65f32c3f340eb6278083c
Marcelo Henrique <77021035+marcelohenrique95@users.noreply.github.com>
2025-12-30 14:26:01 -0400

    fix: remove

--- src/main/java/br/com/coupon/controller/CouponController.java
+++ src/main/java/br/com/coupon/controller/CouponController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/coupon")
 @RequiredArgsConstructor
-@Tag(name = "Cupons", description = "Operações relacionadas a cupons")
+@Tag(name = "Cupons")
 public class CouponController {
 
     private final CouponService service;
